2791b284183fbb7d25ef673d225904db7bba8452
Daniel Sutanto Sutanto <108246001+MoonCaik@users.noreply.github.com>
2022-07-28 21:28:32 +0000

    Rendered platform

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import random
+from platforms import Platforms
 from pygame.locals import*
 
 pygame.init()
@@ -10,11 +11,17 @@ int(screen_info.current_h))
 screen= pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 
-Sprite_List = pygame.sprite.Group()
-Platforms = pygame.sprite.Group()
+sprite_List = pygame.sprite.Group()
+platforms = pygame.sprite.Group()
 
-
+def init():
+  
+  for i in range(height//100):
+    for j in range(width//410):
+      plat = Platforms((random.randint(5, (width - 50) // 10)* 10, 120 * i), 'images/grassHalf.png', 70, 40)
+      platforms.add(plat)
 def main():
+  init()
   while True:
     clock.tick(60)
     for event in pygame.event.get():
@@ -26,8 +33,8 @@ def main():
         if event.key==pygame.K_ESCAPE:
           pygame.display.set_mode(size)
     screen.fill((0,0,100))
-    Platforms.draw(screen)
-    Sprite_List.draw(screen)
+    platforms.draw(screen)
+    sprite_List.draw(screen)
     pygame.display.flip()
 if __name__=="__main__":
   main()
