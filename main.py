2f6507065fcd0bf516d74521855f5d8ace55220d
Daniel Sutanto Sutanto <108246001+MoonCaik@users.noreply.github.com>
2022-07-28 20:51:13 +0000

    added platforms.py

--- main.py
+++ main.py
@@ -10,11 +10,13 @@ int(screen_info.current_h))
 screen= pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 
-
+Sprite_List = pygame.sprite.Group()
+Platforms = pygame.sprite.Group()
 
 
 def main():
   while True:
+    clock.tick(60)
     for event in pygame.event.get():
       if event.type == QUIT: 
         sys.exit()
@@ -24,6 +26,8 @@ def main():
         if event.key==pygame.K_ESCAPE:
           pygame.display.set_mode(size)
     screen.fill((0,0,100))
+    Platforms.draw(screen)
+    Sprite_List.draw(screen)
     pygame.display.flip()
 if __name__=="__main__":
   main()
